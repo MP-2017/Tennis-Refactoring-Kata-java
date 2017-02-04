1ada037b2a797ec11a44658eaa6b6096338a7399
Michele Paterniti <michele.paterniti@jobrapido.com>
2017-02-04 14:45:20 +0100

    Risolto bug in assegnazione dei punti (metodo wonPoint(...)) e risistemati nomi variabili

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,28 +1,30 @@
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int score1 = 0;
+    private int score2 = 0;
+    private final String player1;
+    private final String player2;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (player1.equals(playerName))
+            score1 += 1;
+        else if (player2.equals(playerName))
+            score2 += 1;
+        else 
+            throw new IllegalArgumentException("Invalid player name: " + playerName);
     }
 
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (score1 == score2) {
+            switch (score1) {
             case 0:
                 score = "Love-All";
                 break;
@@ -36,23 +38,23 @@ public class TennisGame1 implements TennisGame {
                 score = "Deuce";
                 break;
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
+        } else if (score1 >= 4 || score2 >= 4) {
+            int minusResult = score1 - score2;
             if (minusResult == 1)
-                score = "Advantage player1";
+                score = "Advantage " + player1;
             else if (minusResult == -1)
-                score = "Advantage player2";
+                score = "Advantage " + player2;
             else if (minusResult >= 2)
-                score = "Win for player1";
+                score = "Win for " + player1;
             else
-                score = "Win for player2";
+                score = "Win for " + player2;
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1)
-                    tempScore = m_score1;
+                    tempScore = score1;
                 else {
                     score += "-";
-                    tempScore = m_score2;
+                    tempScore = score2;
                 }
                 switch (tempScore) {
                 case 0:
