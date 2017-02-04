a98e2dd4f726432d6ff4a2c6aab251bd4cbf1a11
Michele Paterniti <michele.paterniti@jobrapido.com>
2017-02-04 18:08:09 +0100

    Aggiunto test per verificare l'improprio utilizzo della classe TennisGame1 dopo il termine del game

--- src/test/java/TennisGame1Test2.java
+++ src/test/java/TennisGame1Test2.java
@@ -6,11 +6,11 @@ public class TennisGame1Test2 {
 
     @Test
     public void givenATennisGameWhenAssigningAPointToAnUnexistingPlayerThenAnExceptionIsRaised() {
-        TennisGame1 game = new TennisGame1("first player", "second player");
+        TennisGame1 gameUnderTest = new TennisGame1("first player", "second player");
         
         IllegalArgumentException actualException = null; 
         try {
-            game.wonPoint("third player");
+            gameUnderTest.wonPoint("third player");
         } catch (IllegalArgumentException e) {
             actualException = e;
         } finally {
@@ -18,5 +18,92 @@ public class TennisGame1Test2 {
             assertEquals("Invalid player name: third player", actualException.getMessage());
         }
     }
-
+    
+    @Test
+    public void givenPlayer1WonWhenAssigningOneMorePointToPlayer1ThenAnExceptionIsRaised() {
+        TennisGame1 gameUnderTest = new TennisGame1("winner", "loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        
+        IllegalStateException actualException = null; 
+        try {
+            gameUnderTest.wonPoint("winner");
+        } catch (IllegalStateException e) {
+            actualException = e;
+        } finally {
+            assertNotNull(actualException);
+            assertEquals("Game over", actualException.getMessage());
+        }
+    }
+    
+    @Test
+    public void givenPlayer2WonWhenAssigningOneMorePointToPlayer2ThenAnExceptionIsRaised() {
+        TennisGame1 gameUnderTest = new TennisGame1("loser", "winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        
+        IllegalStateException actualException = null; 
+        try {
+            gameUnderTest.wonPoint("winner");
+        } catch (IllegalStateException e) {
+            actualException = e;
+        } finally {
+            assertNotNull(actualException);
+            assertEquals("Game over", actualException.getMessage());
+        }
+    }
+    
+    @Test
+    public void givenPlayer1WonWhenAssigningOneMorePointToThePlayer2ThenAnExceptionIsRaised() {
+        TennisGame1 gameUnderTest = new TennisGame1("winner", "loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        
+        IllegalStateException actualException = null; 
+        try {
+            gameUnderTest.wonPoint("loser");
+        } catch (IllegalStateException e) {
+            actualException = e;
+        } finally {
+            assertNotNull(actualException);
+            assertEquals("Game over", actualException.getMessage());
+        }
+    }
+    
+    @Test
+    public void givenPlayer2WonWhenAssigningOneMorePointToPlayer1ThenAnExceptionIsRaised() {
+        TennisGame1 gameUnderTest = new TennisGame1("loser", "winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("loser");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        gameUnderTest.wonPoint("winner");
+        
+        IllegalStateException actualException = null; 
+        try {
+            gameUnderTest.wonPoint("loser");
+        } catch (IllegalStateException e) {
+            actualException = e;
+        } finally {
+            assertNotNull(actualException);
+            assertEquals("Game over", actualException.getMessage());
+        }
+    }
 }
