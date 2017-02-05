0658695588384e04a35776bb08b442ae3078158e
Michele Paterniti <michele.paterniti@jobrapido.com>
2017-02-05 20:00:44 +0100

    Rinominate classi di test per aderire alle convenzioni di Maven

--- src/test/java/TennisGameATest.java
+++ src/test/java/TennisGameATest.java
@@ -9,7 +9,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class TennisGame1Test1 {
+public class TennisGameATest {
 
     private final String player1Name;
     private final String player2Name;
@@ -17,7 +17,7 @@ public class TennisGame1Test1 {
     private final int player2Points;
     private final String expectedScore;
 
-    public TennisGame1Test1(String player1Name, int player1Points, String player2Name, int player2Points, String expectedScore) {
+    public TennisGameATest(String player1Name, int player1Points, String player2Name, int player2Points, String expectedScore) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
         this.player1Points = player1Points;
--- src/test/java/TennisGameBTest.java
+++ src/test/java/TennisGameBTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TennisGame1Test2 {
+public class TennisGameBTest {
 
     @Test
     public void givenANullPlayer1WhenCreatingANewGameThenAnExceptionIsRaised() {
